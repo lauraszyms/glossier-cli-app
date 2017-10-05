971ef45cb068244d1e63a5780ae46eb835accfd1
lauraszyms <lauraszyms@gmail.com>
2017-10-05 12:01:04 -0700

    stubbed out catagories

--- lib/glossier/cli.rb
+++ lib/glossier/cli.rb
@@ -2,6 +2,13 @@ class Glossier::CLI
 
   def call
     puts "Glossier - A Beauty Brand Inspired by Real Life"
+    list_catagories
+  end
+
+  def list_catagories
+    puts "1. Skincare"
+    puts "2. Makeup"
+    puts "3. Body"
   end
 
 end
