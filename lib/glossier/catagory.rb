24d8f2bcb443cc75b09c18b17745d69f9b6cda7c
lauraszyms <lauraszyms@gmail.com>
2017-10-08 23:13:24 -0700

    refactoring syntax

--- lib/glossier/catagory.rb
+++ lib/glossier/catagory.rb
@@ -14,49 +14,49 @@ class Glossier::Catagory
   end
 
 
-   def self.all
-     @@all
-   end
+  def self.all
+   @@all
+  end
 
-   def clear
-     @@all.clear
-   end
+  def clear
+   @@all.clear
+  end
 
-   def self.list
-     @@all.each_with_index do |catagory, index|
-       puts "#{index + 1}. #{catagory.name.capitalize}"
-     end
+  def self.list
+   @@all.each_with_index do |catagory, index|
+     puts "#{index + 1}. #{catagory.name.capitalize}"
    end
+  end
 
-   def add_product(product)
-     @products << product
-   end
+  def add_product(product)
+   @products << product
+  end
 
-   def self.find_by_name(name)
-     all.detect { |a| a.name == name }
-   end
+  def self.find_by_name(name)
+   all.detect { |a| a.name == name }
+  end
 
-   def self.create(name)
-     if self.find_by_name = nil
-     new_instance = self.new(name)
-     @@all << new_instance
-     new_instance
-    end
+  def self.create(name)
+   if self.find_by_name = nil
+    new_instance = self.new(name)
+    @@all << new_instance
+    new_instance
    end
+  end
 
-   def self.find_by_name(name)
-     all.detect { |a| a.name == name }
-   end
+  def self.find_by_name(name)
+   all.detect { |a| a.name == name }
+  end
 
-   def self.find_or_create_by_name(name)
-     self.find_by_name(name) || self.create(name)
-   end
+  def self.find_or_create_by_name(name)
+   self.find_by_name(name) || self.create(name)
+  end
 
-   def list_products
-     @products.uniq.each_with_index do |product, index|
-       puts "#{index +1}. #{product.name}"
-     end
+  def list_products
+   @products.uniq.each_with_index do |product, index|
+     puts "#{index +1}. #{product.name}"
    end
+  end
 
 
 end
--- lib/glossier/cli.rb
+++ lib/glossier/cli.rb
@@ -25,27 +25,27 @@ class Glossier::CLI
     puts "Enter the number of the catagory you would like to explore, or type exit.".colorize(:light_magenta)
     catagory_choice = nil
     while catagory_choice != "exit"
-       catagory_choice = gets.chomp.downcase
+      catagory_choice = gets.chomp.downcase
       if catagory_choice.to_i > 0
        Glossier::Catagory.all[catagory_choice.to_i - 1].list_products
        puts "Enter the number of the product you would like to explore, or type exit.".colorize(:light_magenta)
        product_choice = gets.chomp
        Glossier::Catagory.all[catagory_choice.to_i - 1].list_products[product_choice.to_i - 1].list_attributes
-      menu_reset
-     end
+       menu_reset
+      end
     end
   end
 
   def menu_reset
     puts "Would you like to continue to explore, y or n?".colorize(:light_magenta)
     input = gets.chomp.downcase
-     while input != "exit"
+    while input != "exit"
      if input == "y"
        call
      elsif input == "n"
        self.goodbye
-        end
-      end
+     end
+    end
    end
 
   def goodbye
